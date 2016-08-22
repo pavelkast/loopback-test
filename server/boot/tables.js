ebc4da58f7645973d2ed7712702188ce585d79e4
Pavel Kastsiukou <pkastsiukou@klika-tech.com>
2016-08-22 16:51:02 +0300

    Models MySQL migrate added. HTML added

---
+++ server/boot/tables.js
@@ -0,0 +1,26 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-getting-started
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+module.exports = function(app) {
+  app.dataSources.mysqlDs.automigrate('User', function(err) {
+    if (err) throw err;
+  });
+
+  app.dataSources.mysqlDs.automigrate('AccessToken', function(err) {
+    if (err) throw err;
+  });
+
+  app.dataSources.mysqlDs.automigrate('ACL', function(err) {
+    if (err) throw err;
+  });
+
+  app.dataSources.mysqlDs.automigrate('RoleMapping', function(err) {
+    if (err) throw err;
+  });
+
+  app.dataSources.mysqlDs.automigrate('Role', function(err) {
+    if (err) throw err;
+  });
+};
